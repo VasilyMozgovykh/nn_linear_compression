82cf742c264fd999824ab19a961f662d51b2af90
VasilyMozgovykh <vasily.mozgovykh@yandex.ru>
2023-11-04 16:12:01 +0300

    Новая рисовалка для распределения сингулярных чисел

--- utils/factorizations.py
+++ utils/factorizations.py
@@ -1,7 +1,17 @@
+from matplotlib import pyplot as plt
 import torch
 
-def plot_singular_value_distribution(matrix):
-    pass
+
+def plot_singular_value_cdf(matrix):
+    _, S, _ = torch.svd(matrix, compute_uv=False)
+    index = torch.arange(len(S) + 1)
+    singular_value_cdf = [0.0] + list(torch.cumsum(S, dim=0) / torch.sum(S))
+    plt.figure(figsize=(10, 8))
+    plt.ylim(0, 1)
+    plt.yticks(torch.linspace(0, 1, 11))
+    plt.plot(index, singular_value_cdf, linewidth=2, color='red')
+    plt.grid(True)
+    plt.show()
 
 def get_cur_decomposition(matrix):
     pass
